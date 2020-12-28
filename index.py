a09c43a8859d9372f01f63d204e7174924524235
Yuto Watanabe <yuto.w51942@gmail.com>
2020-12-28 17:53:29 +0900

    classをグローブ化した

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from unchi import Analysis
 app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False
+TOKENIZE = Analysis()
 
 
 @app.route('/')
@@ -14,8 +15,7 @@ def main():
   formated_text = ''
 
   if text != '' and text is not None:
-    tokenize = Analysis()
-    formated_text = ''.join(tokenize.change_noun(text, 'うんち'))
+    formated_text = ''.join(TOKENIZE.change_noun(text, 'うんち'))
 
   return {"formatted_text": formated_text}
 
